2722d0abcc964ed24f83c6289f5ab589db32fc2b
Zach S <disasterpiece802@gmail.com>
2019-05-08 16:38:23 -0400

    Create GuessWindow.java

---
+++ Projects/GuessingGame/GuessWindow.java
@@ -0,0 +1,14 @@
+package guessingGame;
+
+public class GuessWindow {
+
+	public static void main(String[] args) {
+
+		// Create application frame
+		MyGuessFrame frame = new MyGuessFrame();
+		// Show frame.
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+
+}
